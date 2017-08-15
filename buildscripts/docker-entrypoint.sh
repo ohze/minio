4abbde675ae2178d3dacf50097ab35f0c04bbe2e
thanhbv <thanhbv@sandinh.net>
2017-08-15 14:14:47 +0700

    fix issue: in rancher with docker 1.11.2 (not occur on docker 1.12.1), minio1 can not ping minio2 so docker-entrypoint.sh hang on docker_wait_hosts function
    fix by adding SERVICE_N_STACK env variable, ex: minio3.minio

--- buildscripts/docker-entrypoint.sh
+++ buildscripts/docker-entrypoint.sh
@@ -22,6 +22,59 @@ if [ "${1}" != "minio" ]; then
     fi
 fi
 
+# fix issue: in rancher with docker 1.11.2 (not occur on docker 1.12.1), minio1 can not ping minio2
+# so this script hang on docker_wait_hosts
+# fix by adding SERVICE_N_STACK env variable, ex: minio3.minio
+# todo remove when update docker 1.12+ (then we should use the original minio/minio image)
+fix_resolv() {
+    if [ "$SERVICE_N_STACK" != "" ]; then
+        STACK=${SERVICE_N_STACK/*./}
+        sed /etc/resolv.conf \
+            -e "s|^search .*|search ${STACK}.rancher.internal rancher.internal ${SERVICE_N_STACK}.rancher.internal|" \
+            > /tmp/resolv.conf
+        cat /tmp/resolv.conf > /etc/resolv.conf
+    fi
+}
+
+# note: `docker_wait_hosts` logic in this script is moved to cmd/net.go in commit
+# d4b107ad: Retry name lookup for kubernetes and docker swarm environment (#4800)
+
+# Wait for all the hosts to come online and have
+# their DNS entries populated properly.
+docker_wait_hosts() {
+    hosts="$@"
+    num_hosts=0
+    # Count number of hosts in arguments.
+    for host in $hosts; do
+        [ $(echo "$host" | grep -E "^http") ] || continue
+        num_hosts=$((num_hosts+1))
+    done
+    if [ $num_hosts -gt 0 ]; then
+        echo -n "Waiting for all hosts to resolve..."
+        while true; do
+            fix_resolv
+            x=0
+            for host in $hosts; do
+                [ $(echo "$host" | grep -E "^http") ] || continue
+                # Extract the domain.
+                host=$(echo $host | sed -e 's/^http[s]\?:\/\/\([^\/]\+\).*/\1/')
+                echo -n .
+                val=$(ping -c 1 $host 2>/dev/null)
+                if [ $? != 0 ]; then
+                    echo "Failed to lookup $host"
+                    continue
+                fi
+                x=$((x+1))
+            done
+            # Provided hosts same as successful hosts, should break out.
+            test $x -eq $num_hosts && break
+            echo "Failed to resolve hosts.. retrying after 1 second."
+            sleep 1
+        done
+        echo "All hosts are resolving proceeding to initialize Minio."
+    fi
+}
+
 ## Look for docker secrets in default documented location.
 docker_secrets_env() {
     local MINIO_ACCESS_KEY_FILE="/run/secrets/access_key"
@@ -39,5 +92,8 @@ docker_secrets_env() {
 
 ## Set access env from secrets if necessary.
 docker_secrets_env
+
+## Wait for all the hosts to come online.
+docker_wait_hosts "$@"
 
 exec "$@"
